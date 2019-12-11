2170b131ed559c8802a75b6e9280da702e1a8442
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-12-11 11:31:00 +0800

    更新代码

--- src/main/java/study/springcloud/client/feign/support/SpringBootCfg.java
+++ src/main/java/study/springcloud/client/feign/support/SpringBootCfg.java
@@ -2,12 +2,10 @@ package study.springcloud.client.feign.support;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-//@RibbonClient
 @EnableEurekaClient
 @EnableFeignClients(basePackages = {"study.springcloud.provider"})
-@SpringBootApplication(scanBasePackages = "study.springcloud.consumer")
+@SpringBootApplication(scanBasePackages = "study.springcloud.client.feign")
 public class SpringBootCfg {
 }
---
+++ src/main/java/study/springcloud/client/feign/support/feign/FeignCfg.java
@@ -0,0 +1,4 @@
+package study.springcloud.client.feign.support.feign;
+
+public class FeignCfg {
+}
