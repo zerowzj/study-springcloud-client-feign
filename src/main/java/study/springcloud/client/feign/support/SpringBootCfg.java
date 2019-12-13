16a944a90e9b6b0f7605a741c7070c4dafaf4d71
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-12-13 12:33:31 +0800

    更新代码

--- src/main/java/study/springcloud/client/feign/support/SpringBootCfg.java
+++ src/main/java/study/springcloud/client/feign/support/SpringBootCfg.java
@@ -2,7 +2,6 @@ package study.springcloud.client.feign.support;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import study.springcloud.client.feign.support.ribbon.RibbonCfg;
 
