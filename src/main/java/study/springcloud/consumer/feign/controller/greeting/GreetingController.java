95b284250d5702002116a213f1a0f6197160e758
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-11-03 11:10:26 +0800

    更新代码

--- src/main/java/study/springcloud/consumer/feign/controller/greeting/GreetingController.java
+++ src/main/java/study/springcloud/consumer/feign/controller/greeting/GreetingController.java
@@ -1,4 +1,4 @@
-package study.springcloud.consumer.feign.controller;
+package study.springcloud.consumer.feign.controller.greeting;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
