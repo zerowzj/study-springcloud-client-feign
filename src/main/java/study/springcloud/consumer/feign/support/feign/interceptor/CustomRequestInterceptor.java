015e283877758dc95fae5df520382168f3c08490
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-11-05 18:18:21 +0800

    更新代码

--- src/main/java/study/springcloud/consumer/feign/support/feign/interceptor/CustomRequestInterceptor.java
+++ src/main/java/study/springcloud/consumer/feign/support/feign/interceptor/CustomRequestInterceptor.java
@@ -7,13 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.openfeign.ribbon.LoadBalancerFeignClient;
 
 /**
- * 可以在构造RequestTemplate 请求时，增加或者修改Header, Method, Body 等信息
+ * 可以在构造RequestTemplate 请求时，增加或者修改Header, Method, Body等信息
  */
 @Slf4j
 public class CustomRequestInterceptor implements RequestInterceptor {
 
     @Autowired
-    LoadBalancerFeignClient loadBalancerFeignClient;
+    private LoadBalancerFeignClient loadBalancerFeignClient;
 
     @Override
     public void apply(RequestTemplate requestTemplate) {
